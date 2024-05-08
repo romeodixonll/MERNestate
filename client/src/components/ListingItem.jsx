e8d4519a426e03f393c6087c81cd76ffeb304a9d
Romeo Dixon <romeodixon2nd@gmail.com>
2024-05-08 14:56:57 -0500

    complete home page

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -14,13 +14,13 @@ export default function ListingItem({ listing }) {
           alt="listing cover"
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
-        <div className="p-3 flex flex-col gap-2">
+        <div className="p-3 flex flex-col gap-2 w-full">
           <p className="truncate text-lg font-semibold text-slate-700">
             {listing.name}
           </p>
           <div className="flex items-center gap-1">
             <MdLocationOn className="h-4 w-4 text-green-700" />
-            <p className="text-sm text-gray-600 truncate w-full">
+            <p className="text-sm text-gray-600 truncate">
               {listing.address}
             </p>
           </div>
